3151065f046a56a5964a0214754b4f15883aa71e
zhao <1036978689@qq.com>
2019-11-22 22:50:52 +0800

    init

---
+++ article-service/src/main/java/com/zhao/demo/controller/ArticleController.java
@@ -0,0 +1,18 @@
+package com.zhao.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ArticleController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/article/callHello")
+    public String callHello(){
+        return restTemplate.getForObject("http://eureka-client-user-service/user/hello", String.class);
+    }
+}
