8cc78b3f732c6c2c39e0016a622d83003452d498
zhao <1036978689@qq.com>
2019-11-23 18:22:08 +0800

    feign

--- feign-inherit-api/src/main/java/com/zhao/demo/api/client/UserRemoteClient.java
+++ feign-inherit-api/src/main/java/com/zhao/demo/api/client/UserRemoteClient.java
@@ -1,4 +1,11 @@
 package com.zhao.demo.api.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("feign-inherit-provide")
 public interface UserRemoteClient {
+
+    @GetMapping("/user/name")
+    String getName();
 }
